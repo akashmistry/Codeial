eeb0869a4e00bbf28e43ed0f38a4bca728d4cd7b
Akash <akash.mistry@innodeed.com>
2023-08-19 04:37:43 +0530

    added a router and controller for home page

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 2620;
 
+// USING EXPRESS ROUTER
+app.use("/", require("./routes/index"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log("Oops error in running the sever:", err);
