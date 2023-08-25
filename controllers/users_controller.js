f7cbf0eb6a83e27f36d9012855cd2e48abe469ee
Akash <akash.mistry@innodeed.com>
2023-08-26 01:52:23 +0530

    everything till signup and creating user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,53 @@
+const User = require("../models/user.js");
+const router = require("../routes");
+
+// HOME PAGE CONTROLLER
 module.exports.profile = function (req, res) {
-  return res.render("users", {
+  return res.render("user_profile", {
     title: "Profile",
   });
 };
+
+// USER SIGN UP CONTROLLER
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial| Sign up",
+  });
+};
+
+// USER SIGN IN CONTROLLER
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial| Sign in",
+  });
+};
+
+// GET SIGN UP DATA
+module.exports.create = function (req, res) {
+  console.log(req.body);
+  if (req.body.password != req.body.confirm_password) {
+    console.log("WRONG PASS");
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        User.create(req.body)
+          .then((user) => {
+            res.redirect("/users/sign-in");
+          })
+          .catch((err) => {
+            console.log("Error in fining user in signing up");
+          });
+      } else {
+        res.redirect("back");
+      }
+    })
+    .catch((err) => {
+      console.log("Error in fining user in signing up");
+    });
+};
+
+// SIGN IN AND CREATE SESSION FOR THE USER
+module.exports.createSession = function (req, res) {};
