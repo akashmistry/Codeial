45dfd6d4f3db1006dfd3d353c82436dbea2c6aa1
Akash <akash.mistry@innodeed.com>
2023-09-08 22:28:05 +0530

    authentication added for anonymous user post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
   // COOKIES WILL COME AS REQ
   console.log(req.cookies);
@@ -5,7 +7,17 @@ module.exports.home = function (req, res) {
   // COOKIES WILL GO AS RES
   res.cookie("something", 20);
 
-  return res.render("home", {
-    title: "Home",
-  });
+  // Post.find({});
+
+  Post.find({})
+    .populate("user")
+    .then((posts) => {
+      return res.render("home", {
+        title: "Home",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      console.log("Error in fetching the posts");
+    });
 };
