f207c2ea7695b688b1c06e059d9ebbe374f98e4d
Akash <akash.mistry@innodeed.com>
2023-10-05 08:43:03 +0530

    nodemailer setup and sending first mail cia SMTP with html template

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,26 +2,40 @@ const Comment = require("../models/comments");
 
 const Post = require("../models/post");
 
+const commentsMailer = require("../mailers/comments_mailer");
+
 module.exports.create = async function (req, res) {
   try {
     const post = await Post.findById(req.body.post);
     if (post) {
-      const comment = await Comment.create({
+      let comment = await Comment.create({
         content: req.body.content,
         post: req.body.post,
         user: req.user._id,
       });
 
       post.comments.push(comment);
-      await post.save();
+      post.save();
+
+      comment = await comment.populate("user", "name email");
+      commentsMailer.newComment(comment);
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment: comment,
+          },
+          message: "Post created!",
+        });
+      }
+
       req.flash("success", "Comment Published!");
 
       res.redirect("/");
     }
   } catch (err) {
-    req.flash("error", "Comment cannot be Published!");
-
     console.log(err);
+    req.flash("error", err);
   }
 };
 
@@ -32,10 +46,19 @@ module.exports.destroy = async function (req, res) {
       let postId = comment.post;
 
       await comment.deleteOne();
-
-      await Post.findByIdAndUpdate(postId, {
+      const post = await Post.findByIdAndUpdate(postId, {
         $pull: { comments: req.params.id },
       });
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment_id: req.params.id,
+          },
+          message: "Post deleted",
+        });
+      }
+
       req.flash("success", "Comment Deleted!");
 
       return res.redirect("back");
@@ -43,9 +66,8 @@ module.exports.destroy = async function (req, res) {
       return res.redirect("back");
     }
   } catch (err) {
-    req.flash("error", "Comment cannot beDeleted!");
-
     console.log("Error in finding comment");
+    req.flash("error", "Comment cannot beDeleted!");
     return res.redirect("back");
   }
 };
